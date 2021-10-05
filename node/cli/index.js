1f1f49cc71f68d1f434d73d6a0c3fe4f462ded9d
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-10-05 13:41:18 +0200

    Aufgabe 4 Lösung

--- node/cli/index.js
+++ node/cli/index.js
@@ -7,7 +7,24 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-const tasks = getTasks();
+function parseCli(args) {
+  const result = {
+    numberOfTasks: 4,
+    level: 1,
+  };
+  args.forEach((arg) => {
+    if (arg.startsWith('--number')) {
+      result.numberOfTasks = parseInt(arg.substr(9), 10);
+    } else if (arg.startsWith('--level')) {
+      result.level = parseInt(arg.substr(8), 10);
+    }
+  });
+  return result;
+}
+
+const { numberOfTasks, level } = parseCli(process.argv);
+
+const tasks = getTasks(numberOfTasks, level);
 
 for (let i = 0; i < tasks.length; i++) {
   try {
--- node/cli/task.js
+++ node/cli/task.js
@@ -1,10 +1,10 @@
-export default function getTasks() {
+export default function getTasks(numberOfTasks, level) {
   const tasks = [];
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < numberOfTasks; i++) {
     let task = {
       operands: [
-        Math.floor(Math.random() * 10),
-        Math.floor(Math.random() * 10),
+        Math.floor(Math.random() * (level === 1 ? 9 : 99)),
+        Math.floor(Math.random() * (level < 3 ? 9 : 99)),
       ],
     };
     task.result = task.operands[0] + task.operands[1];
