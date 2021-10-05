2eff61cd1e30980a359f6b325849ec492f19d089
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-10-05 11:56:51 +0200

    Aufgabe 3 Lösung 1

--- node/cli/index.js
+++ node/cli/index.js
@@ -5,17 +5,37 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-const task = {
-  operands: [2, 2],
-  result: 4,
-};
+const tasks = [
+  {
+    operands: [2, 2],
+    result: 4,
+  },
+  {
+    operands: [3, 2],
+    result: 5,
+  },
+];
 
-rl.question(`What is ${task.operands[0]} + ${task.operands[1]}? `, (answer) => {
-  const intAnswer = parseInt(answer, 10);
-  if (intAnswer === task.result) {
-    console.log('Awesome!');
-  } else {
-    console.log('Nope!');
-  }
-  rl.close();
-});
+rl.question(
+  `What is ${tasks[0].operands[0]} + ${tasks[0].operands[1]}? `,
+  (answer) => {
+    const intAnswer = parseInt(answer, 10);
+    if (intAnswer === tasks[0].result) {
+      console.log('Awesome!');
+    } else {
+      console.log('Nope!');
+    }
+    rl.question(
+      `What is ${tasks[1].operands[0]} + ${tasks[1].operands[1]}? `,
+      (answer) => {
+        const intAnswer = parseInt(answer, 10);
+        if (intAnswer === tasks[1].result) {
+          console.log('Awesome!');
+        } else {
+          console.log('Nope!');
+        }
+        rl.close();
+      },
+    );
+  },
+);
