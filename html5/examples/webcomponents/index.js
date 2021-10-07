a80d6209ce88286d13748dbc99b0cd4935a7da8b
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-10-07 11:43:48 +0200

    load template async

--- html5/examples/webcomponents/index.js
+++ html5/examples/webcomponents/index.js
@@ -1,10 +1,24 @@
 class MyComponent extends HTMLElement {
   constructor() {
     super();
+    // this.init();
+    this.initAsync();
+  }
 
+  async initAsync() {
+    const response = await fetch('template.html');
+    const data = await response.text();
+    const template = document.createElement('template');
+    template.innerHTML = data;
+    const templateContent = template.content;
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+
+    shadowRoot.appendChild(templateContent.cloneNode(true));
+  }
+
+  init() {
     const template = document.getElementById('my-component');
     const templateContent = template.content;
-
     const shadowRoot = this.attachShadow({ mode: 'open' });
 
     shadowRoot.appendChild(templateContent.cloneNode(true));
